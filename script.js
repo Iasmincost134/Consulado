c37b591f130c7da65faa6768a42aeb2f876026b9
Iasmincost134 <133365214+Iasmincost134@users.noreply.github.com>
2024-09-05 21:44:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 new Swiper('.card-wrapper', {
     loop: true,
-    spaceBetween: 10,
+    spaceBetween: 30,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
     },
   
     // Navigation arrows
@@ -28,3 +26,18 @@ new Swiper('.card-wrapper', {
         },
     }
   });
+  const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
